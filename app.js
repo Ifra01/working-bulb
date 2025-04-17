e15a105c6fa856e60d2cef8b3000b82f61942f5b
Ifra01 <jamalifra74@gmail.com>
2025-04-17 13:39:01 +0500

    created working bulb

---
+++ app.js
@@ -0,0 +1,8 @@
+// let bulb = document.getElementById("bulb");
+// let buttonOn = document.getElementById("one");
+// let buttonOff = document.getElementById("two");
+
+// buttonOn.addEventListener("click", function(){
+//     bulb.src="./images/"
+// })
+
