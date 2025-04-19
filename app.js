0a721a33929a1f0064f6730a790b6dba92d26958
ifra01 <jamalifra74@gmail.com>
2025-04-19 11:28:56 +0500

    made changes in js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-// let bulb = document.getElementById("bulb");
-// let buttonOn = document.getElementById("one");
-// let buttonOff = document.getElementById("two");
 
-// buttonOn.addEventListener("click", function(){
-//     bulb.src="./images/"
-// })
+let bulb = document.getElementById("off");
+let button_on = document.getElementById("click");
+let button_off = document.getElementById("no-click");
+
+button_on.addEventListener("click", function(){
+    // bulb.className = "size"
+    bulb.src="./images/bulb-on.png"
+})
+
+button_off.addEventListener("click", function(){
+    // bulb.className = "size"
+    bulb.src="./images/bulb-off.png"
+})
+
 
